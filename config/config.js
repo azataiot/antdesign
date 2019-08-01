4f98d80a449b8fe434834f03ee74caa05a408092
azat.ai <azat.ai@azataideMacBook-Air.local>
2019-08-01 23:49:25 +0600

    installed umi-plogin-react

---
+++ config/config.js
@@ -0,0 +1,13 @@
+export default {
+    plugins:[
+        ['umi-plugin-react', {
+        //这里是plugin 的一些具体内容
+        }],
+    ],
+    routes: [
+        {
+            path:'/',
+            component:'./HelloWorld',
+        }
+    ],
+};
