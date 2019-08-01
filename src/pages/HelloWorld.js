d876c2c0a428c7e4121377092ec2cbdde7c2eaf1
azat.ai <azat.ai@azataideMacBook-Air.local>
2019-08-02 00:53:14 +0600

    组件

--- src/pages/HelloWorld.js
+++ src/pages/HelloWorld.js
@@ -1,3 +1,24 @@
+import React from 'react';
+import ShoppingList from "./shoppinglist";
+import Square from "./buttontest";
+
+const Picture = (props) =>{
+    return (
+        <div>
+            <img src="{ props.src }"/>
+            {props.children}
+        </div>
+    )
+};
+
+const picture = {
+    src: 'https://cdn.nlark.com/yuque/0/2018/jpeg/84141/1536207007004-59352a41-4ad8-409b-a416-a4f324eb6d0b.jpeg'};
+
+
+
 export default () => {
-    return <div>hello world</div>;
+    return(
+        <Square>
+        </Square>
+    );
 }
