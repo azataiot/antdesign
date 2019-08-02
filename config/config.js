0a1795d3eae8ea22ad6d3111e2862d2990f6c88c
azat.ai <azat.ai@azataideMacBook-Air.local>
2019-08-02 10:17:56 +0600

    update from air last

--- config/config.js
+++ config/config.js
@@ -1,13 +1,14 @@
 export default {
     plugins:[
         ['umi-plugin-react', {
+        antd:true
         //这里是plugin 的一些具体内容
         }],
     ],
     routes: [
         {
             path:'/',
-            component:'./HelloWorld',
+            component:'./home',
         }
     ],
 };
